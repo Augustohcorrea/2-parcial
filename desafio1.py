96e838ffe2aaa3a567652602ded56d3255bebff1
Augustohcorrea <augusto.hobi.correa@escola.pr.gov.br>
2026-04-04 17:05:11 -0300

    desafio1.py

--- desafio1.py
+++ desafio1.py
@@ -1,4 +1,11 @@
+# Pede ao usuário o primeiro número e converte para decimal (float)
 num1 = float(input("Digite o primeiro número: "))
+
+# Pede o segundo número e também converte para decimal
 num2 = float(input("Digite o segundo número: "))
+
+# Soma os dois números e guarda o resultado na variável 'soma'
 soma = num1 + num2
+
+# Mostra o resultado na tela usando f-string (permite inserir variáveis no texto)
 print(f"A soma de {num1} e {num2} é: {soma}")
