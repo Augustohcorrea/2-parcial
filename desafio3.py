e41428dcb3df5532869314cfae936d0ad0be2601
Augustohcorrea <augusto.hobi.correa@escola.pr.gov.br>
2026-04-04 17:04:34 -0300

    desafio3.py

--- desafio3.py
+++ desafio3.py
@@ -1,5 +1,13 @@
+# Cria uma lista com vários nomes
 nomes = ["Andre", "Bruna", "Cleiton", "Fausto", "Turu"]
+
+# Mostra a lista inteira
 print("Lista de nomes:", nomes)
+
+# Pula uma linha (\n) e mostra um título
 print("\nImprimindo cada nome:")
+
+# Loop que percorre cada item da lista
 for nome in nomes:
+    # A cada repetição, imprime um nome da lista
     print(nome)
